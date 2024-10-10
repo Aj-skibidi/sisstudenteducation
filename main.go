241db4bfa6cd85782abc6d3c04faac4d0b0f2a23
Aj-skibidi <1109192@lcps.org>
2024-10-10 15:35:27 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "surfdoge.pro", nil
+            return "https://kazwire.com/", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
